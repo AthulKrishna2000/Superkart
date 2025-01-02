e82e4aa94db4d1ab7c1f2b3b2962c65b74cee2d0
AthulKrishna2000 <athul997788@gmail.com>
2025-01-02 18:40:23 +0530

    name update

--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppConstant {
-  static String appMainName = 'Ecomm';
+  static String appMainName = 'Ecomm1';
   static String appPoweredBy = 'powerd by athul';
   static const appMainColor = Color(0xFFbf1b08);
   static const appSecendoryColor = Color(0xFF981206);
